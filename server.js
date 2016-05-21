9f4f2db5b97ddfae3ee3570505113ed67333d365
Josemar Magalhaes <josemarmagalhaes@hotmail.com>
2016-05-20 20:40:56 -0700

    setup validations with Joi

--- server.js
+++ server.js
@@ -1,11 +1,20 @@
 var Hapi = require('hapi');
 var uuid = require('uuid');
+var fs = require('fs');
+var Joi = require('joi');
 
 var server = new Hapi.Server();
 
-var cards = {};
+var cards = loadCards();
 
 server.connection({port: 3000});
+
+server.views({
+  engines: {
+    html: require('handlebars')
+  },
+  path: './templates'
+})
 
 server.ext('onRequest', function(request, reply) {
   console.log(request.method.toUpperCase(),
@@ -50,37 +59,58 @@ server.route({
   handler: deleCardHandler
 })
 
+var cardSchema = Joi.object().keys({
+  name: Joi.string().min(3).max(50).required(),
+  recipient_email: Joi.string().email().required(),
+  sender_name: Joi.string().min(3).max(50).required(),
+  sender_email: Joi.string().email().required(),
+  card_image: Joi.string().regex(/.+\.(jpg|bmp|png|gif)\b/).required()
+})
+
 function newCardHandler(request, reply) {
   if (request.method === 'get') {
-    reply.file('templates/new.html')
+    reply.view('new', {card_images: mapImages()})
   } else {
-    var card = {
-      name: request.payload.name,
-      email: request.payload.recipient_email,
-      sender_name: request.payload.sender_name,
-      sender_email: request.payload.sender_email,
-      card_image: request.payload.card_image
-    }
-    saveCard(card);
-
-    console.log(card);
-
-    reply.redirect('/cards');
+    Joi.validate(request.payload, cardSchema, function(err, val) {
+      if (err) {
+        console.log(err);
+        return reply(err);
+      }
+      var card = {
+        name: val.name,
+        email: val.recipient_email,
+        sender_name: val.sender_name,
+        sender_email: val.sender_email,
+        card_image: val.card_image
+      }
+      saveCard(card);
+      reply.redirect('/cards');
+    })
   }
 }
 
 function cardsHandler(request, reply) {
-  reply.file('templates/cards.html')
+  reply.view('cards', {cards: cards})
 }
 
 function deleCardHandler(request, reply) {
   delete cards[request.params.id];
+  reply()
 }
 
 function saveCard(card) {
   var id = uuid.v1();
   card.id = id;
   cards[id] = card;
+}
+
+function loadCards() {
+  var file = fs.readFileSync('./cards.json');
+  return JSON.parse(file.toString());
+}
+
+function mapImages() {
+  return fs.readdirSync('./public/images/cards');
 }
 server.start(function() {
   console.log('Server running at', server.info.uri);
